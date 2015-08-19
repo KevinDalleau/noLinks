5d391c0ec7fcac8a4e4d183fedb5b8dcbb524d2e
Kevin Dalleau <kevin.dalleaugmail.com>
2015-08-20 01:14:41 +0200

    Beginning odf get-pair function

--- nolinks/src/nolinks/core.clj
+++ nolinks/src/nolinks/core.clj
@@ -9,15 +9,20 @@
     (with-open [file (io/reader fname)]
       (csv/parse-csv (slurp file) :delimiter \tab)))
 
+  (defn get-pair
+    "Returns all gene-drug pairs."
+    [genes-set drugs-set]
+    (str (nth genes-set 1) "-" (nth drugs-set 1))
+    )
 
 (defn -main
   [& args]
-  (def drugs-set (set nil))
-  (def genes-set (set nil))
+  (def drugs-set (list nil))
+  (def genes-set (list nil))
   (doseq [keyval (take-csv "drugs.tsv")] (def drugs-set (conj drugs-set (get keyval 0)))
   )
   (doseq [keyval (take-csv "genes.tsv")] (def genes-set (conj genes-set (get keyval 0)))
   )
-  (prn drugs-set)
-  (prn genes-set)
+  (get-pair genes-set drugs-set)
+
   )
